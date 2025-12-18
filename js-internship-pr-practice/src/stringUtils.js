faad89aa9e41d6192f77d7cd2efe72f9c2d52302
hiteshi <hitu1@email.com>
2025-12-18 21:19:40 +0530

    Fixed arrayUtils, mathUtils, stringUtils

--- js-internship-pr-practice/src/stringUtils.js
+++ js-internship-pr-practice/src/stringUtils.js
@@ -1,6 +1,8 @@
 // ISSUE: Convert string to uppercase
 function toUpperCase(str) {
-    return str;
+    if (typeof str !== "string") return "";
+
+    return str.toUpperCase();
 }
 
 module.exports = { toUpperCase };
