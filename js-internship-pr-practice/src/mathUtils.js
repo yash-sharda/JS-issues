e36f96fbd37354c8c515fdc862a73d3db73514b9
VinayMarabe <vinaymarbe40@gmail.com>
2025-12-18 21:07:50 +0530

    New features

--- js-internship-pr-practice/src/mathUtils.js
+++ js-internship-pr-practice/src/mathUtils.js
@@ -1,6 +1,25 @@
-// ISSUE: Implement multiply function
+// Returns the product of two numbers
 function multiply(a, b) {
-    return 0;
+    return a * b;
 }
 
 module.exports = { multiply };
+
+// NEW FEATURE 1: Returns the factorial of a number
+function factorial(n) {
+    if (typeof n !== "number" || n < 0 || !Number.isInteger(n)) {   
+        throw new TypeError("Input must be a non-negative integer");
+    }
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+
+
+    return result;
+}
+module.exports.factorial = factorial;
