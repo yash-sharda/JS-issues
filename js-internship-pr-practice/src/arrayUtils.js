fa3c2d3dccc3b8627cca577962d61a52237b8535
atharvaM89 <atharvamahulkar12@gmail.com>
2025-12-18 21:06:40 +0530

    Entire repo is fixed

--- js-internship-pr-practice/src/arrayUtils.js
+++ js-internship-pr-practice/src/arrayUtils.js
@@ -1,6 +1,9 @@
 // ISSUE: Return sum of array elements
+//code fixed
 function sumArray(arr) {
-    return 0;
+   if (!Array.isArray(arr)) return 0;
+
+    return arr.reduce((sum, val) => sum + val, 0);
 }
 
 module.exports = { sumArray };
