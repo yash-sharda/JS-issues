78fcefd079334feac5b40217f6afe00bfdaba5e1
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-12-18 20:59:29 +0530

    Solved all JS issues + imported and tested

--- js-internship-pr-practice/src/index.js
+++ js-internship-pr-practice/src/index.js
@@ -1,2 +1,15 @@
+const { sumArray } = require('./arrayUtils');
+const { multiply } = require('./mathUtils');
+const { toUpperCase } = require('./stringUtils');
+
+console.log("Sum array: ");
+console.log(sumArray([1, 2, 3, 4, 5]));
+
+console.log("Multiply: ");
+console.log(multiply(5, 3));
+
+console.log("Testing uppercase:");
+console.log(toUpperCase("hello"))
+
 console.log("JS Internship PR Practice Repo");
 
