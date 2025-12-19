605d7e2a5094036a64928328bd9fa7e257a48680
Rushikesh Thorat <rushikeshthorat189@gamil.com>
2025-12-19 19:52:20 +0530

    Convert string to uppercase in stringUtils

--- js-internship-pr-practice/src/stringUtils.js
+++ js-internship-pr-practice/src/stringUtils.js
@@ -1,6 +1,10 @@
 // ISSUE: Convert string to uppercase
 function toUpperCase(str) {
-    return str;
+    if (typeof str !== "string") {
+        throw new Error("Input must be a string");
+    }
+
+    return str.toUpperCase();
 }
 
 module.exports = { toUpperCase };
